6400c00c208187bb9dcacc269d6048416658751e
Mihiolus <108616993+Mihiolus@users.noreply.github.com>
2022-07-05 09:15:09 +0300

    Implemented finisher move

--- Assets/AnimationIntegration/Scripts/MouseLook.cs
+++ Assets/AnimationIntegration/Scripts/MouseLook.cs
@@ -7,18 +7,28 @@ public class MouseLook : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed = 180f, _maxAngle = 90f;
     private float _previousAngle = 0f;
+    public bool FollowMouse = true;
 
     public void LateUpdate()
     {
-        var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(mouseRay, out hit))
+        if (FollowMouse)
         {
-            var mousePos = hit.point;
-            var lookDir = Vector3.ProjectOnPlane(mousePos - _targetBone.position, Vector3.up);
-            var angle = Vector3.SignedAngle(transform.forward, lookDir, Vector3.up);
-            angle = Mathf.Clamp(angle,-_maxAngle,_maxAngle);
-            angle = Mathf.MoveTowards(_previousAngle, angle,_rotationSpeed*Time.deltaTime);
+            var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(mouseRay, out hit))
+            {
+                var mousePos = hit.point;
+                var lookDir = Vector3.ProjectOnPlane(mousePos - _targetBone.position, Vector3.up);
+                var angle = Vector3.SignedAngle(transform.forward, lookDir, Vector3.up);
+                angle = Mathf.Clamp(angle, -_maxAngle, _maxAngle);
+                angle = Mathf.MoveTowards(_previousAngle, angle, _rotationSpeed * Time.deltaTime);
+                _targetBone.rotation = Quaternion.AngleAxis(angle, Vector3.up) * _targetBone.rotation;
+                _previousAngle = angle;
+            }
+        }
+        else
+        {
+            var angle = Mathf.MoveTowards(_previousAngle, 0, _rotationSpeed * Time.deltaTime);
             _targetBone.rotation = Quaternion.AngleAxis(angle, Vector3.up) * _targetBone.rotation;
             _previousAngle = angle;
         }
--- Assets/AnimationIntegration/Scripts/Player.cs
+++ Assets/AnimationIntegration/Scripts/Player.cs
@@ -1,19 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 5f;
+    private float _speed = 5f, _finisherApproachSpeed = 10f;
     [SerializeField]
     private Animator _animator;
     [SerializeField]
-    private float _rotationSpeed = 180f;
+    private float _rotationSpeed = 180f, _finisherRotationSpeed = 720f;
     [SerializeField]
     private Transform _enemy;
     [SerializeField]
-    private float _killDistance = 2f;
+    private float _finisherPromptDistance = 2f;
     [SerializeField]
-    private GameObject _finishPrompt;
+    private GameObject _finisherPrompt;
+    [SerializeField]
+    private float _finisherDistance = 1f, _finisherAngleError = 5f;
+    public bool FinisherRunning;
+    private MouseLook _mouseLook;
+    [SerializeField]
+    private GameObject _gun, _sword;
 
     private void Awake()
     {
@@ -21,11 +28,24 @@ public class Player : MonoBehaviour
         {
             _animator = GetComponentInChildren<Animator>();
         }
+        _mouseLook = GetComponent<MouseLook>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (FinisherRunning)
+        {
+            return;
+        }
+        var distToEnemy = (_enemy.position - transform.position).sqrMagnitude;
+        bool finisherAvailable = distToEnemy <= _finisherPromptDistance * _finisherPromptDistance;
+        _finisherPrompt.SetActive(finisherAvailable);
+        if (finisherAvailable && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Finisher());
+        }
+
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
         bool isRunning = !(Mathf.Approximately(x, Mathf.Epsilon)
@@ -45,8 +65,54 @@ public class Player : MonoBehaviour
             _animator.SetFloat("Move X", orientationDelta.x);
             _animator.SetFloat("Move Y", orientationDelta.z);
         }
+    }
 
-        var distToEnemy = (_enemy.position - transform.position).sqrMagnitude;
-        _finishPrompt.SetActive(distToEnemy<=_killDistance*_killDistance);
+    private IEnumerator Finisher()
+    {
+        FinisherRunning = true;
+        _mouseLook.FollowMouse = false;
+        Vector3 distVector = _enemy.position - transform.position;
+        float dist = distVector.magnitude;
+        
+        bool mustApproach = dist > _finisherDistance;
+        float angleDelta = Vector3.Angle(distVector, transform.forward);
+        var velocity = distVector.normalized * _finisherApproachSpeed;
+        if(!mustApproach){
+            velocity *= -1f;
+        }
+
+        //if starts outside the finisher radius, come closer
+        //if starts inside the finisher radius, step back
+        while ((mustApproach && dist > _finisherDistance)
+         || (!mustApproach && dist < _finisherDistance)
+         || angleDelta > _finisherAngleError)
+        {
+            _animator.SetBool("Running", true);
+            if ((mustApproach&& dist > _finisherDistance)
+            ||(!mustApproach && dist < _finisherDistance))
+            {
+                transform.position += velocity * Time.deltaTime;
+            }
+            var targetRotation = Quaternion.LookRotation(distVector, Vector3.up);
+            transform.rotation =
+             Quaternion.RotateTowards(transform.rotation, targetRotation, _finisherRotationSpeed * Time.deltaTime);
+            var orientationDelta = transform.InverseTransformDirection(velocity.normalized);
+            _animator.SetFloat("Move X", orientationDelta.x);
+            _animator.SetFloat("Move Y", orientationDelta.z);
+            distVector = _enemy.position - transform.position;
+            dist = distVector.magnitude;
+            angleDelta = Vector3.Angle(distVector, transform.forward);
+            yield return null;
+        }
+        _animator.SetBool("Running", false);
+        _gun.SetActive(false);
+        _sword.SetActive(true);
+        _animator.SetTrigger("Finish");
+        while(FinisherRunning){
+            yield return null;
+        }
+        _gun.SetActive(true);
+        _sword.SetActive(false);
+        _mouseLook.FollowMouse = true;
     }
 }
